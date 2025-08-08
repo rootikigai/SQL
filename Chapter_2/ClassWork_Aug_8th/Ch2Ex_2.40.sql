33f243da730156510ab2813e5c31981029f7f6b3
rootikigai <ifykel360@gmail.com>
2025-08-08 15:49:58 +0100

    Ch2Ex40

---
+++ Chapter_2/ClassWork_Aug_8th/Ch2Ex_2.40.sql
@@ -0,0 +1,3 @@
+Select SKU, SKU_Description, INVENTORY.WarehouseID, WarehouseCity, WarehouseState
+FROM INVENTORY, WAREHOUSE
+where WarehouseCity = "Atlanta" or WarehouseCity = "Bangor" or WarehouseCity = "Chicago";
